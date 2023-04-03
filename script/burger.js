e500737b360a452ac217a5543fc826a3d183be1f
Grigoriy5544 <gregkotsenko@yandex.ru>
2023-04-03 19:32:50 +0400

    fix bag

--- script/burger.js
+++ script/burger.js
@@ -1,14 +1,18 @@
-let menuBtn = document.querySelector('.burger-btn');
-let menu = document.querySelector('.mobile-menu');
+let menuBtn = document.querySelector('.burger-btn')
+let menu = document.querySelector('.mobile-menu')
 menuBtn.addEventListener('click', function () {
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('active');
+	menuBtn.classList.toggle('active')
+	menu.classList.toggle('active')
 })
 
-//фикс бага бургер меню при большой ширине
 window.addEventListener('resize', () => {
-    if (window.innerWidth > 1024 && menu.classList.contains('active')) {
-        menuBtn.classList.remove('active')
-        menu.classList.remove('active')
-    }
-});
+	if (window.innerWidth > 1024 && menu.classList.contains('active')) {
+		menuBtn.classList.remove('active')
+		menu.classList.remove('active')
+	}
+})
+
+window.addEventListener('scroll', () => {
+	menuBtn.classList.remove('active')
+	menu.classList.remove('active')
+})
